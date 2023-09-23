a06ba4c3f50ca289e8f2c4e2ecb91c4cfd2b5d4e
Jeslia Rose <jesliarosejoy@gmail.com>
2023-09-24 01:23:45 +0530

    change

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -4,9 +4,25 @@ import { useAddTransaction } from "../../hooks/useAddTransactions";
 import { useGetTransactions } from "../../hooks/useGetTransactions";
 import { useGetUserInfo } from "../../hooks/useGetUserInfo";
 import { useNavigate } from "react-router-dom";
-
 import "./dashboard.css";
-import { auth } from "../../config/firebase-config";
+import logo from "../../images/logo.png";
+import petcare from "../../images/petcare.png";
+import car from "../../images/car.png";
+import computer from "../../images/computer.png";
+import education from "../../images/education.png";
+import food from "../../images/food.png";
+import health from "../../images/health.png";
+import house from "../../images/house.png";
+import shopping from "../../images/shopping.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSackDollar,
+  faArrowRightArrowLeft,
+  faIndianRupeeSign,
+} from "@fortawesome/free-solid-svg-icons";
+import { db } from "../../config/firebase-config"; 
+import { auth } from "../../config/firebase-config"; 
+
 
 export const SmartWallet = () => {
   const { addTransaction } = useAddTransaction();
@@ -44,6 +60,110 @@ export const SmartWallet = () => {
 
   return (
     <>
+    <div className="whole">
+      <div className="category">
+        <div className="logo">
+          <img src={logo} height="34px" width="249px" alt="Logo" />
+        </div>
+        <div className="categorylogo">
+          <div className="icons">
+            <a href="/formdata">
+              <img src={house} alt="" />
+            </a>
+            <img src={car} alt="" />
+          </div>
+          <div className="icons">
+            <img src={education} alt="" />
+            <img src={health} alt="" />
+          </div>
+          <div className="icons">
+            <img src={food} alt="" />
+            <img src={shopping} alt="" />
+          </div>
+          <div className="icons">
+            <img src={computer} alt="" />
+            <img src={petcare} alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="containers">
+        <div className="header">
+          <h2 className="left_align">Dashboard</h2>
+          <div className="profile">
+            <p className="person">Welcome {name}!</p>
+           <img className="profile-photo" src={profilePhoto} />
+          </div>
+        </div>
+        <div className="budget">
+          <h4>YOUR BUDGET SUMMARY</h4>
+        </div>
+        <div className="total">
+          <div className="box">
+            <h4>
+              <FontAwesomeIcon icon={faSackDollar} /> Total Income
+            </h4>
+            <p>
+              <b>INR {income}/-</b>
+            </p>
+          </div>
+          <div className="box">
+            <h4>
+              <FontAwesomeIcon icon={faArrowRightArrowLeft} /> Total Expense
+            </h4>
+            <p>
+              <b>INR {expenses}/-</b>
+            </p>
+          </div>
+          <div className="box">
+            <h4>
+              <FontAwesomeIcon icon={faIndianRupeeSign} /> Balance
+            </h4>
+            <p>
+              <b>INR {balance}/-</b>
+            </p>
+          </div>
+        </div>
+        <div className="big">
+          <div className="text-main">
+            <div className="text">
+              <p>
+                Expense <b>INR 2300 </b>than last month
+              </p>
+            </div>
+            <div className="text">
+              <p>
+                Savings <b>INR 2510 </b> than last month
+              </p>
+              <div className="scroll">
+              <h3> Transactions</h3>
+        <ul>
+          {transactions.map((transaction) => {
+            const { description, transactionAmount, transactionType } =
+              transaction;
+            return (
+              <li>
+                <h4> {description} </h4>
+                <p>
+                  ${transactionAmount} •{" "}
+                  <label
+                    style={{
+                      color: transactionType === "expense" ? "red" : "green",
+                    }}
+                  >
+                    {" "}
+                    {transactionType}{" "}
+                  </label>
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
       <div className="expense-tracker">
         <div className="container">
           <h1> {name}'s Expense Tracker</h1>
